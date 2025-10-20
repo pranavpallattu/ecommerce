c558760efa41240f1a65212cfff5c5996e55a75b
pranavpallattu <pranavpspallattu@gmail.com>
2025-10-20 21:07:29 +0530

    feat: editCoupon Api

--- src/controllers/couponController.js
+++ src/controllers/couponController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Coupon = require("../models/couponSchema");
 const {
   validateCouponData,
@@ -26,7 +27,7 @@ exports.addCouponController = async (req, res) => {
     if (existingCoupon) {
       return res
         .status(409)
-        .json({ success: false, message: "Coupon Already Exists" });
+        .json({ success: false, message: "Coupon already exist" });
     }
 
     const couponData = {
@@ -60,7 +61,87 @@ exports.addCouponController = async (req, res) => {
   }
 };
 
+exports.editCouponController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
 
+    if (!id || !mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid coupon ID",
+      });
+    }
+
+    const existingCoupon = await Coupon.findById(id);
+    if (!existingCoupon) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Coupon does not exist" });
+    }
+
+    if (Object.keys(updateData).length === 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: "No Fields provided for update" });
+    }
+
+    try {
+      validateEditCouponData(updateData);
+    } catch (validationError) {
+      return res
+        .status(400)
+        .json({ success: false, message: validationError.message });
+    }
+    if (updateData.code && updateData.code !== existingCoupon.code) {
+      const existingCouponCode = await Coupon.findOne({
+        code: updateData.code.toUpperCase().trim(),
+        _id: { $ne: id },
+      });
+      if (existingCouponCode) {
+        return res.status(409).json({
+          success: false,
+          message: "Another coupon with this code already exist",
+        });
+      }
+
+      updateData.code = updateData.code.toUpperCase().trim();
+    }
+
+    if (updateData.discount !== undefined)
+      updateData.discount = Number(updateData.discount);
+
+    if (updateData.minPurchase !== undefined)
+      updateData.minPurchase = Number(updateData.minPurchase);
+
+    if (updateData.usageLimit !== undefined && updateData.usageLimit !== null) {
+      updateData.usageLimit = Number(updateData.usageLimit);
+    }
+
+    if (
+      updateData.perUserLimit !== undefined &&
+      updateData.perUserLimit !== null
+    ) {
+      updateData.perUserLimit = Number(updateData.perUserLimit);
+    }
+
+    const updatedCoupon = await Coupon.findByIdAndUpdate(
+      id,
+      {
+        $set: updateData,
+      },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Coupon updated successfully",
+      data: updatedCoupon,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 exports.updateCouponStatusController = async (req, res) => {
   try {
