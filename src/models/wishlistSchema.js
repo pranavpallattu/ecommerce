94136f012acbcbb181618b17876a4c0a63345b32
pranavpallattu <pranavpspallattu@gmail.com>
2025-10-22 21:23:10 +0530

    feat: wishlist Schema

---
+++ src/models/wishlistSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    UserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        addedOn: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Wishlist = mongoose.model("Wishlist", wishlistSchema);
+module.exports = Wishlist;
