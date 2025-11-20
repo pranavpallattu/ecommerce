9cb2953d34904c83880d9232b11324b5be3dfca0
pranavpallattu <pranavpspallattu@gmail.com>
2025-11-20 14:33:35 +0530

    feat(dashboard): get order summary api

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,13 +1,42 @@
+const Order = require("../models/orderSchema");
 
+exports.getOrderSummary = async (req, res) => {
+  try {
+    const orderSummary = await Order.aggregate([
+      {
+        $group: {
+          _id: "$orderStatus",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
 
-exports.getOrderSummary=async(req,res)=>{
-    try{
+    const summary = {
+      totalOrders: 0,
+      delivered: 0,
+      processing: 0,
+      confirmed: 0,
+      pending: 0,
+      cancelled: 0,
+      partiallyCancelled: 0,
+      shipped: 0,
+      returned: 0,
+      partiallyReturned: 0,
+      returnPending: 0,
+      returnRejected: 0,
+    };
 
-        
-        
-    }
-    catch (error) {
+    orderSummary.forEach((item) => {
+      summary.totalOrders += item.count;
+      summary[item._id.toLowerCase()] = item.count;
+    });
+
+    res.status(200).json({
+      success: true,
+      data: summary,
+    });
+  } catch (error) {
     console.error("Error fetching order summary:", error);
     return res.status(500).json({ success: false, message: error.message });
   }
-}
+};
