a6efa885de4b33b15310330dbbd032b1060901c3
pranavpallattu <pranavpspallattu@gmail.com>
2026-03-16 22:08:32 +0530

    fix: google callbackurl

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID, // from Google Cloud Console
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-callbackURL: "http://localhost:7777/api/auth/google/callback",    },
+callbackURL: "https://ecommerceui-one.vercel.app/api/auth/google/callback",    },
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({
