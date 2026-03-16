e8ad3e67d4ef23517728ce636fdf13eb73b84bf9
pranavpallattu <pranavpspallattu@gmail.com>
2026-03-16 18:24:29 +0530

    added origin

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ require("./config/passport");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ecommerceui-one.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
