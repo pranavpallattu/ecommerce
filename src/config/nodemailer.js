d13e48d0db19112f14eb9e1558209d5b5c87caac
pranavpallattu <pranavpspallattu@gmail.com>
2026-03-17 15:16:16 +0530

    fix: nodemailer port

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,14 +1,14 @@
-const nodemailer=require("nodemailer")
+const nodemailer = require("nodemailer");
 
-const transporter=nodemailer.createTransport({
-    secure:true,
-    host:"smtp.gmail.com",
-    port:465,
-    auth:{
-        user:"pranavpallattu2004@gmail.com",
-        pass:"oxiwipuobylaiwvn"
-    }
-})
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // ❗ MUST be false for 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 async function sendOtpEmail(toEmail, otp) {
   const info = await transporter.sendMail({
